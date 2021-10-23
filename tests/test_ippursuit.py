ff882dd63b0e96f930251d980454a73db7509971
Mike R <cr8syoki@gmail.com>
2021-10-24 06:53:25 +0900

    add tests

--- tests/test_ippursuit.py
+++ tests/test_ippursuit.py
@@ -1,5 +1,13 @@
 from ippursuit import __version__
-
+from ippursuit.config_setup import CONFIG
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_greynoise_equals_200():
+    key = CONFIG['greynoise']['api']
+    headers = {'key': key}
+    r = requests.request("GET", https://greynoise.io/v3/community/1.1.1.1', headers=headers)
+
+    assert r.headers['Content-Type'] == 'application/json'
